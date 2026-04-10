c4edf859ebe7d5d5422206d6d58c039fc0a9d2b2
xavimagine <sr.xavier5@gmail.com>
2026-04-10 21:24:44 +0200

    3 intento

--- app.js
+++ app.js
@@ -11,12 +11,23 @@ import listasRoutes from "./routes/listRaoutes.js"; // Verifica si es "listRoute
 const app = express();
 
 // Configuración de CORS
-app.use(
-    cors({
-        origin: (origin, callback) => callback(null, true),
-        credentials: true,
-    }),
-);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS",
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    );
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
