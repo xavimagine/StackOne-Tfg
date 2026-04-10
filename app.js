b2bb4c7d21caed1f8df78b28f0aacb3a299dbe1d
xavimagine <sr.xavier5@gmail.com>
2026-04-10 21:01:54 +0200

    finish

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ const app = express();
 app.use(
     cors({
         origin: [
-            process.env.FRONTEND_URL || "http://localhost:5173",
             "http://localhost:3000",
+            "http://localhost:5173",
             "http://127.0.0.1:3000",
+            process.env.FRONTEND_URL,
         ],
         credentials: true,
     }),
