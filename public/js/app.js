1407d81e94102796c753778eec0ad6e67239cb6c
xavimagine <sr.xavier5@gmail.com>
2026-04-15 12:14:57 +0200

    ajustes visuales.

--- public/js/app.js
+++ public/js/app.js
@@ -413,7 +413,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function renderCards(games) {
         const container = document.getElementById("resultados");
         container.className =
-            "grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4";
+            "grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 p-4";
         container.innerHTML = "";
 
         games.forEach((game) => {
@@ -880,7 +880,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 const grid = document.createElement("div");
                 grid.className =
-                    "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4 w-full";
+                    "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4 w-full";
 
                 result.games.forEach((game) => {
                     const card = createGameCard({
