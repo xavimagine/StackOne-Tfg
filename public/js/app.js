78da7afdc50f006aaec6e589f54bb1ba56ec957c
xavimagine <sr.xavier5@gmail.com>
2026-04-28 16:49:56 +0200

    fixed listas

--- public/js/app.js
+++ public/js/app.js
@@ -934,7 +934,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 const grid = document.createElement("div");
                 grid.className =
-                    "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4 w-full";
+                    "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2  lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6 p-4 w-full";
 
                 result.games.forEach((game) => {
                     const card = createGameCard({
